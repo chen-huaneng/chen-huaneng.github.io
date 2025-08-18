90152b508d2b5aa3149b359af8d94b4640f7e14c
chen-huaneng <huanengchen@foxmail.com>
2025-08-18 16:20:02 +0800

    Site updated: 08-Aug 18, 2025 16:19:57

--- js/duration.js
+++ js/duration.js
@@ -19,12 +19,8 @@
       mnum = "0" + mnum;
     }
     seconds = (now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
-    snum = Math.round(seconds);
-    if(String(snum).length === 1 ){
-      snum = "0" + snum;
-    }
     document.getElementById("timeDate").innerHTML = "本站已安全运行&nbsp"+dnum+"&nbsp天";
-    document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
+    document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp";
   }
 
   update();
